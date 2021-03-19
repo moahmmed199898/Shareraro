02014d1fe6129a7a71cbbf07bad765260b21d5dd
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2021-03-19 14:37:35 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AnswerPage from "./pages/AnswerPage";
 import CallPage from "./pages/CallPage";
 import "./style.scss";
@@ -6,10 +7,16 @@ export default class App extends React.Component {
 	
 	render() {
 		return (
-			<div className="App">
-				<CallPage />
-				<AnswerPage />
-			</div>
+			<Router>
+				<Switch>
+					<Route path="/call"><CallPage /></Route>
+					<Route path="/answer"><AnswerPage /></Route>
+				</Switch>
+			</Router>
+			// <div className="App">
+			// 	<CallPage />
+			// 	<AnswerPage />
+			// </div>
 		);
 	}
 }
