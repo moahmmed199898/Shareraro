5039163c4a969ba440bc50c55beb3f0dc493d51c
Mohammed Alkhafaji <alkhafml@miamioh.edu>
2021-03-23 22:06:02 -0400

    starting to add some UI

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,16 +1,37 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./_indexPage.scss"
+import Card from "../../components/Cards/Card";
+import {faDesktop, faDatabase} from "@fortawesome/free-solid-svg-icons"
 export default class IndexPage extends React.Component {
     render() {
         return (
-            <div className="nav">
-                <Link className={"link"} to={"/call"}>
+            <div className="IndexPage">
+                <h1 className="title">Shareraro</h1>
+                <p className="description">Share your screen and send large files to your friends with one button <br/>
+                What do you want to do?
+                </p>
+                <div className="cards">
+                    <Card
+                        title="Screen Share"
+                        icon={faDesktop}
+                        description="Share your screen in high quality with all your friends"
+                        color="greenBackground"
+                    />
+                    <Card
+                        title="Data Transfer"
+                        icon={faDatabase}
+                        description="Send large files to your friends easily"
+                        color="purpleBackground"
+                    />
+                </div>
+
+
+                {/* <Link className={"link"} to={"/call"}>
                     <button className={"call"}>Call</button>
                 </Link>
                 <Link className={"link"} to={"/answer"}>
                     <button className={"answer"}>answer</button>
-                </Link>
+                </Link> */}
             </div>
         )
     }
